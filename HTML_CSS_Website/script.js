54b076e2bdb9fcee8c5d87b6fe2e701880d16eb8
sheheryarKhan2003 <sheheryarkhan01011@gmail.com>
2024-11-10 15:42:10 +0500

    Add files via upload

---
+++ HTML_CSS_Website/script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector ('#menu-icon');
+let navbar = document.querySelector ('.navbar');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backened Developer','Graphic Designer','Web Designer'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop:true,
+  });
